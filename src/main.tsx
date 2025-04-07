e68543cc728e2299fc70b5dac1f6b367779e2c14
Хримли Евгения <evichkanova@list.ru>
2025-04-07 11:38:08 +0300

    change routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  MemoryRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.ts";
 import "./index.css";
@@ -13,39 +9,29 @@ import ErrorPage from "./pages/ErrorPage.tsx";
 import HeroPage from "./components/Heroes/HeroPage.tsx";
 import CreateHero from "./pages/CreateHero/CreateHero.tsx";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: <App />,
-      errorElement: <ErrorPage />,
-    },
-    {
-      path: "/hero/:id",
-      element: <HeroPage />,
-    },
-    {
-      path: "/create",
-      element: <CreateHero />,
-    },
-  ]
-  // createRoutesFromElements(
-  //   <Route path="/" element={<App />} errorElement={<ErrorPage />}>
-  //     <Route path="/hero/:id" element={<HeroPage />} />
-  //     <Route path="/create" element={<CreateHero />} />
-  //   </Route>
-  // )
-  // {
-  //   basename: "/heroes/",
-  // }
-);
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/hero/:id",
+    element: <HeroPage />,
+  },
+  {
+    path: "/create",
+    element: <CreateHero />,
+  },
+];
+const router = createBrowserRouter(routes, {
+  basename: "/heroes/",
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-      <MemoryRouter basename="/heroes/">
-        <RouterProvider router={router} />
-      </MemoryRouter>
+      <RouterProvider router={router} />
     </Provider>
   </StrictMode>
 );
